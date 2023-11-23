19eaf80f7b0db02d5dfce71df43ef5b5e50c9ede
boouke <cityfluxkontakt@gmail.com>
2023-11-23 23:53:07 +0100

    init

---
+++ src/ErrorMessagesConstants.java
@@ -0,0 +1,3 @@
+public class ErrorMessagesConstants {
+    public static final String msg0x01 = " - The input is null, or otherwise marked as 0 bytes. The program is unable to handle it. Please contact us";
+}
